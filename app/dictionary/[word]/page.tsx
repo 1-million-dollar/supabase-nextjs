1d089d2ae57c006164b14f6a2c283ea71f3a3dc9
Rohan Bhakat <rohanbhakat@Rohans-MacBook-Air.local>
2024-12-06 09:28:57 +0530

    updated 6/12 - 1

--- app/dictionary/[word]/page.tsx
+++ app/dictionary/[word]/page.tsx
@@ -1,110 +1,20 @@
-import { createClient } from "@/utils/supabase/server";
 
-import { searchMeanings } from "@/app/lib/data";
-import { addSearchedWord } from "@/app/lib/data";
 
-import SearchBox from "@/app/ui/searchbox";
-import Meanings from "@/app/ui/meanings";
+import WordMeaning from '@/app/ui/wordmeaning';
 
-import { notFound } from "next/navigation";
-
-import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
-
-type Definition = {
-    definition: string;
-    synonyms?: string;
-    antonyms?: string;
-    example?: string;
-  };
   
-  type Meaning = {
-    partOfSpeech: string;
-    definitions: Definition[];
-  };
 
-  type PageProps = {
-    params: {
-      word: string;
-    };
-  };
+export default function Page({params} : {params: { word: string}}) {
+   
+    const word = params.word; 
 
 
-export default async function Page({ params }: PageProps ) {
 
-    const {word} = await params
-    const data_meaning = await searchMeanings(word)
-    if (!data_meaning || data_meaning.length === 0 || data_meaning.title === "No Definitions Found") {
-        return notFound();
-      }
+  
 
-    const supabase = createClient()
-    const {
-      data: { user },
-    } = await (await supabase).auth.getUser()
-    
-    //console.log(user?.id)
-
-      
-    const { data, error } = await (await supabase)
-        .from('words')
-        .select('userID')
-        .eq('word', word);
-
-    if (error) {
-        console.log(error)
-    }
-
-    if (data && data[0]?.userID === user?.id) {
-        console.log('Duplicate found. Record already exists.', data);
-    } else {
-       
-    const { error: insertError } = await (await supabase)
-        .from('words')
-        .insert([{ userID: user?.id, word: word }]);
-
-    if (insertError) {
-        console.error('Error inserting data:', insertError.message);
-    } else {
-        addSearchedWord(word)
-        console.log('Data inserted successfully.');
-        // create questions of the searched words
-        }
-    }
-    
-    const phoneticsText = data_meaning[0]?.phonetics?.[0]?.text || "";
-    
-    return (
-        <div>
-            <div className="flex justify-center p-2 md:p-5">
-                <SearchBox />
-            </div>
-            <div className="flex items-center p-2 md:p-5">
-                <div className="text-xl md:text-4xl font-bold">
-                    {word}
-                </div>
-                <div className="flex flex-row items-center ml-auto">
-                    {phoneticsText ?? ""}
-                    <SpeakerWaveIcon className="w-8 p-1" />
-                </div>
-            </div>
-
-            
-            {data_meaning[0].meanings.map((meaning: Meaning, index: number) => (
-                <div key={index} className="flex flex-col gap-4">
-                    {meaning.definitions.map((definition: Definition, defindex: number) => (
-
-                        <Meanings key={defindex} 
-                            pos={meaning.partOfSpeech} 
-                            def={definition.definition}
-                            syn={definition.synonyms ?? ""}
-                            ant={definition.antonyms ?? ""}
-                            example={definition.example ?? ""}
-                        />
-                
-                    ))}
-                </div>
-            ))}
-        </div>
-    )
-
+  return(
+    <div>
+        <WordMeaning word={word} />
+    </div>
+  )
 }
---
+++ app/ui/wordmeaning.tsx
@@ -0,0 +1,97 @@
+import { notFound } from "next/navigation";
+import { addSearchedWord, searchMeanings } from "../lib/data";
+import { createClient } from "@/utils/supabase/client";
+import SearchBox from "./searchbox";
+import { SpeakerWaveIcon } from "@heroicons/react/16/solid";
+import Meanings from "./meanings";
+
+type Definition = {
+    definition: string;
+    synonyms?: string;
+    antonyms?: string;
+    example?: string;
+  };
+  
+  type Meaning = {
+    partOfSpeech: string;
+    definitions: Definition[];
+  };
+
+
+export default async function WordMeaning({ word }: { word: string }) {
+    const data_meaning = await searchMeanings(word)
+    if (!data_meaning || data_meaning.length === 0 || data_meaning.title === "No Definitions Found") {
+        return notFound();
+      }
+
+    const supabase = createClient()
+    const {
+      data: { user },
+    } = await (await supabase).auth.getUser()
+    
+    //console.log(user?.id)
+
+      
+    const { data, error } = await (await supabase)
+        .from('words')
+        .select('userID')
+        .eq('word', word);
+
+    if (error) {
+        console.log(error)
+    }
+
+    if (data && data[0]?.userID === user?.id) {
+        console.log('Duplicate found. Record already exists.', data);
+    } else {
+       
+    const { error: insertError } = await (await supabase)
+        .from('words')
+        .insert([{ userID: user?.id, word: word }]);
+
+    if (insertError) {
+        console.error('Error inserting data:', insertError.message);
+    } else {
+        addSearchedWord(word)
+        console.log('Data inserted successfully.');
+        // create questions of the searched words
+        }
+    }
+    
+    const phoneticsText = data_meaning[0]?.phonetics?.[0]?.text || "";
+    
+    return (
+        <div>
+            <div className="flex justify-center p-2 md:p-5">
+                <SearchBox />
+            </div>
+            <div className="flex items-center p-2 md:p-5">
+                <div className="text-xl md:text-4xl font-bold">
+                    {word}
+                </div>
+                <div className="flex flex-row items-center ml-auto">
+                    {phoneticsText ?? ""}
+                    <SpeakerWaveIcon className="w-8 p-1" />
+                </div>
+            </div>
+
+            
+            {data_meaning[0].meanings.map((meaning: Meaning, index: number) => (
+                <div key={index} className="flex flex-col gap-4">
+                    {meaning.definitions.map((definition: Definition, defindex: number) => (
+
+                        <Meanings key={defindex} 
+                            pos={meaning.partOfSpeech} 
+                            def={definition.definition}
+                            syn={definition.synonyms ?? ""}
+                            ant={definition.antonyms ?? ""}
+                            example={definition.example ?? ""}
+                        />
+                
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
+
+}
