7b01d874fde8e5f43202e8d1b3e35f86060090a5
Rohan Bhakat <rohanbhakat@Rohans-MacBook-Air.local>
2024-12-06 12:04:21 +0530

    updated 6/12 -5

--- app/dictionary/page.tsx
+++ app/dictionary/page.tsx
@@ -4,43 +4,11 @@ import Link from "next/link";
 
 export default function Page() {
 
-    const addedWords = [
-        "winding down",
-        "cancanning",
-        "victimism",
-        "five-alarm",
-        "ticket gate",
-        "geotracking",
-        "bierwurst",
-        "waka jump",
-        "bifold",
-        "Grus",
-        "kar seva",
-        "pyrocumulus",
-        "theatrician",
-        "beer baron",
-        "windmilled",
-        "anaesthetics",
-      ]
+    const addedWords = [  "geotracking",  "south-going",  "amphoriskos",  "Indo-Islamic",  "anti-nuke",  "appendicula",  "Basilosaurus",  "winding hole",  "flightworthy",  "stop-start",  "chewiness",  "underinvest",  "kapa haka",  "flipping",  "Indo-jazz",  "clued-in"]
+
       
-    const updatedWords = [
-       "cable car",
-  "clubmoss",
-  "ticket",
-  "Kolam",
-  "wound",
-  "cadre",
-  "clary",
-  "amitosis",
-  "excurse",
-  "estated",
-  "bracing",
-  "fleecy",
-  "argute",
-  "brace",
-  "wind fan",
-  "Elfin",
-      ]
+    const updatedWords = [  "amphora",  "lido",  "corella",  "chattily",  "anti-grav",  "amplify",  "annotine",  "dime",  "staging",  "annuloid",  "upwind",  "excursus",  "amphibii",  "victim",  "kin",  "clary"]
+
       
 
     return (
@@ -51,9 +19,9 @@ export default function Page() {
             <div className="flex flex-col justify-center mb-5 p-5 items-center text-center bg-gray-200 rounded-lg">
                 <p className="text-2xl mb-1"><b>Word of the day</b></p>
                 <div className="h-1 w-10 mb-3 bg-black"></div>
-                <p className="font-extrabold text-4xl mb-1">rantoone</p>
-                <p className="text-sm mb-3">NOUN</p>
-                <p className="text-md">A type of tricycle used in the 19th cent., propelled both by foot pedals and hand-operated levers.</p>
+                <p className="font-extrabold text-4xl mb-1">candy</p>
+                <p className="text-sm mb-3">verb</p>
+                <p className="text-md">To preserve edible plants, fruits, etc. by boiling with sugar, which crystallizes and forms a crust.</p>
             </div>
             <div className="flex flex-col gap-10 md:gap-40 md:flex-row md:justify-center p-5 text-center bg-gray-200 rounded-lg">
                 <div className="flex flex-col justify-center items-center">
